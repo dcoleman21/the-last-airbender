3f7c52813984f8bd319bd48420821dddfd43276c
Dani Coleman <danicoleman00@gmail.com>
2020-12-11 11:01:18 -0700

    Create search controller with index action; make api call for @members

---
+++ app/controllers/search_controller.rb
@@ -0,0 +1,12 @@
+class SearchController < ApplicationController
+  def index
+    nation = params[:nation].gsub('_', ' ')
+
+    conn = Faraday.new(url: "https://last-airbender-api.herokuapp.com")
+
+    response = conn.get("/api/v1/characters") do |req|
+      req.params[:affiliation] = nation
+    end
+    @members = JSON.parse(response.body, symbolize_names: true)
+  end
+end
